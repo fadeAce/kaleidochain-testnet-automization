a08a43317172adb4bd2ebbccf72f86f03df8fee0
terrill <314156936@qq.com>
2019-01-29 14:55:53 +0800

    deploy automization

--- auto_gen_node.sh
+++ auto_gen_node.sh
@@ -10,12 +10,13 @@ do
     fi
     # find H
     H=$(docker exec node1 geth --testnet attach --exec eth.blockNumber)
-    $(ssh root@${IP} "mkdir -p /usr/local/kaleido/testnet/node${seq}")
+    ssh root@${IP} "mkdir -p /usr/local/kaleido/testnet/node${seq}"
     res=$(initnode ${IP} ${SEQ} ${ADDR} ${H})
     echo "result for node is "${res}
     if [[ ${res} -eq 0 ]]; then
     # none fail here and inside ,directly start up
-    ssh root@${IP} "export NODE_NAME='${NAME}${SEQ}';export CONTAINER='node'${SEQ};wget https://raw.githubusercontent.com/fadeAce/kaleidochain-testnet-automization/master/testnet.sh;chmod +x testnet.sh;bash testnet.sh"
+    echo "NODE_NAME='${NAME}';KPORT=388$[${SEQ}+2];CONTAINER='node'${SEQ};ADDRESS=${ADDR}"
+    ssh root@${IP} "export ADDRESS=${ADDR};export NODE_NAME='${NAME}';export KPORT=388$[${SEQ}+2];export CONTAINER='node'${SEQ};wget https://raw.githubusercontent.com/fadeAce/kaleidochain-testnet-automization/master/testnet.sh;chmod +x testnet.sh;bash testnet.sh"
     fi
 done < ${filename}
 
@@ -24,15 +25,15 @@ function initnode() {
     ip=$1
     seq=$2
     addr=$3
-    ssh root@${IP} "mkdir -p /usr/local/kaleido/node${seq}"
-    exist=$(ssh root@${IP} "if [ -d "/usr/local/kaleido/testnet/node${seq}/geth/ephemeralkey-${addr}-0-1000000" ];then
+    exist=$(ssh root@${IP} "if [ -d "/usr/local/kaleido/testnet/node${seq}/testnet/geth/ephemeralkey-${addr}-0-1000000" ];then
         echo 'exist'")
     h=$4
     if [[ "$exist" == "exist" ]]; then
         return 1
     fi
-    /tmp/node${seq}.dat.$$ < ssh root@${IP} "docker run --rm -v /usr/local/kaleido/testnet/node${seq}:/root/.ethereum kaleidochain/client:test --testnet makeminerkey --miner ${addr} --coinbase ${addr} --begin ${h}|grep minerkey"
+    /tmp/node${seq}.dat.$$ < ssh root@${IP} "docker run --rm -v /usr/local/kaleido/testnet/node${seq}:/root/.ethereum kaleidochain/client:test --testnet makeminerkey --miner ${addr} --coinbase ${addr} --begin ${h}"
     JS=`cat /tmp/node${seq}.dat.$$ | gawk '/miner:/{miner=$2;printf("personal.unlockAccount(\"%s\", \"1234\", 3000); ", miner);} /contract address/{minerdb=$3;} /Generate minerkey/{data=$3; printf("hash=eth.sendTransaction({from:\"%s\", to:\"%s\", gas:200000, gasPrice:20000000000, data:\"%s\"});admin.sleepBlocks(2);receipt=eth.getTransactionReceipt(hash);console.log(JSON.stringify(receipt, null, \"\\t\")); hash;\n",miner,minerdb,data)}'`
+    cat /tmp/node${seq}.dat.$$
     rm /tmp/node${seq}.dat.$$
 
     docker exec node1 geth --testnet attach --exec "${JS}"
--- testnet.sh
+++ testnet.sh
@@ -11,10 +11,15 @@ cd node1
 mkdir testnet
 cd testnet
 mkdir geth
-mkdir keystore
 
+mkdir -p /usr/local/kaleido/testnet/${CONTAINER}/testnet/geth
 #prepare compose file
 cd /usr/local/kaleido/testnet
+
+if [[ -a docker-compose.yml ]];then
+rm -f docker-compose.yml
+fi;
+
 touch docker-compose.yml
 
 echo 'version: "2"' >> docker-compose.yml
@@ -23,12 +28,11 @@ echo '  turn:' >> docker-compose.yml
 echo '    image: kaleidochain/client:test' >> docker-compose.yml
 echo '    container_name: node1' >> docker-compose.yml
 echo '    ports:' >> docker-compose.yml
-echo '      - "2510:2510"' >> docker-compose.yml
-echo '      - "38883:38883"' >> docker-compose.yml
-echo '      - "38883:38883/udp"' >> docker-compose.yml
+echo '      - "${KPORT}:${KPORT}"' >> docker-compose.yml
+echo '      - "${KPORT}:${KPORT}/udp"' >> docker-compose.yml
 echo '    volumes:' >> docker-compose.yml
 echo '      - $PWD/node1:/root/.ethereum' >> docker-compose.yml
-echo "    entrypoint: geth -tnode enode://a5989375caad943d5c42a1c2e129cadacb94db2392d3aa22ee081f05f66bfb580a86d3b14f320399ecf6b3348ad06284a6ed038c44cda25630ef4027e7102a06@45.77.42.61:9992  --testnet --rpc --rpcaddr=0.0.0.0 --rpcvhosts='*' --rpcapi eth,net --ws --wsaddr=0.0.0.0 --wsorigins='*' --verbosity 3 --vmodule 'eth/*=5,consensus/*=5,core/*=5,miner/*=5,p2p/discv5/*=3,p2p/discover/*=3,p2p/*=5'" >> docker-compose.yml
+echo "    entrypoint: geth -tnode enode://a5989375caad943d5c42a1c2e129cadacb94db2392d3aa22ee081f05f66bfb580a86d3b14f320399ecf6b3348ad06284a6ed038c44cda25630ef4027e7102a06@45.77.42.61:9992  --testnet --mine --etherbase ${ADDRESS} --gcmode archive --ethstats '${NODE_NAME}:1234@45.77.42.61:30303' --verbosity 3 --vmodule 'eth/*=5,consensus/*=5,core/*=5,miner/*=5,p2p/discv5/*=3,p2p/discover/*=3,p2p/*=5'" >> docker-compose.yml
 
 #prepare compose itself
 pip install docker-compose
